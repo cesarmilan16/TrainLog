ede263611434df28c579bbfa0d3447fd539619b6
cesarmilan16 <cesar3bfermi@gmail.com>
2026-02-18 19:48:01 +0100

    fix db

--- backend/data/db-path.js
+++ backend/data/db-path.js
@@ -1,5 +1,5 @@
 const path = require('path');
 
-const DB_PATH = path.join(__dirname, '..', 'trainlog.db');
+const DB_PATH = process.env.DB_PATH || path.join(__dirname, '..', 'trainlog.db');
 
 module.exports = { DB_PATH };
